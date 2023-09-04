914c9022872da98f70e63130248ebfc22fab84a6
Sofia Donlucas Bañuelos <71408638+Soff17@users.noreply.github.com>
2023-09-04 14:58:21 -0600

    Changes on principal board

--- SEL4C/ViewControllerTableroInicial.swift
+++ SEL4C/ViewControllerTableroInicial.swift
@@ -8,31 +8,57 @@
 import UIKit
 
 class ViewControllerTableroInicial: UIViewController {
-
-    @IBOutlet weak var btnRecursos: UIButton!
-    @IBOutlet weak var btnAct1: UIButton!
     @IBOutlet weak var imageChange: UIImageView!
+    
+    @IBOutlet weak var btnRecursos: UIButton!
     @IBOutlet weak var btnEvaluaciones: UIButton!
     @IBOutlet weak var btnActividades: UIButton!
+    
+    @IBOutlet weak var btnEvaluacionFinal: UIButton!
+    @IBOutlet weak var btnValorSocial: UIButton!
+    @IBOutlet weak var btnAct2: UIButton!
+    @IBOutlet weak var btnEmprendimiento: UIButton!
+    @IBOutlet weak var btnAct1: UIButton!
+    
+    @IBOutlet weak var btnEvaluacionInicial: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        btnEvaluacionInicial.isEnabled = false
+        btnEvaluacionFinal.isEnabled = false
+        btnEmprendimiento.isEnabled = false
+        btnValorSocial.isEnabled = false
         // Do any additional setup after loading the view.
     }
     
     @IBAction func btn1Pressed(_ sender: UIButton) {
         imageChange.image = UIImage(named: "6 - tableroEvaluaciones")
         btnAct1.isEnabled = true
-        }
+        btnAct2.isEnabled = true
+        btnEvaluacionInicial.isEnabled = false
+        btnEvaluacionFinal.isEnabled = false
+        btnEmprendimiento.isEnabled = false
+        btnValorSocial.isEnabled = false
+    }
         
     @IBAction func btn2Pressed(_ sender: UIButton) {
         imageChange.image = UIImage(named: "6.8 - tableroEv")
         btnAct1.isEnabled = false
+        btnAct2.isEnabled = false
+        btnEvaluacionInicial.isEnabled = true
+        btnEvaluacionFinal.isEnabled = true
+        btnEmprendimiento.isEnabled = false
+        btnValorSocial.isEnabled = false
     }
     
     @IBAction func btn3Pressed(_ sender: UIButton) {
         imageChange.image = UIImage(named: "6.9 - tableroRecursos")
         btnAct1.isEnabled = false
+        btnAct2.isEnabled = false
+        btnEvaluacionInicial.isEnabled = false
+        btnEvaluacionFinal.isEnabled = false
+        btnEmprendimiento.isEnabled = true
+        btnValorSocial.isEnabled = true
     }
     
     /*
